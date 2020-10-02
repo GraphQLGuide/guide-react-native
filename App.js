a958f89618be7deed24de9fa990b62549d39bcf1
Loren 🤓 <lorensr@gmail.com>
2020-10-02 17:38:52 -0400

    Configure swipe-back gesture to work starting anywhere on the screen

--- App.js
+++ App.js
@@ -55,6 +55,7 @@ export default function App() {
               },
             }) => ({
               title: number ? `Chapter ${number}: ${title}` : title,
+              gestureResponseDistance: { horizontal: 500 },
             })}
           />
         </Stack.Navigator>
